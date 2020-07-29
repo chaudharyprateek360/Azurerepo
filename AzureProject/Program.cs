e4f8be25047ecf6237aa2f1b567af48f29a56664
unknown <Prateek1.Choudhary@lntinfotech.com>
2020-07-29 10:25:27 +0530

    azure

---
+++ AzureProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("iN Azure");
+        }
+    }
+}
